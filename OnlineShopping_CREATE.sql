25cd40bb3a31f199dcd06b32956a2f48c1f08e20
ejarredondo <ejarredondo@mail.lipscomb.edu>
2025-09-30 10:25:32 -0500

    Made format consistent for all create statements

--- OnlineShopping_CREATE.sql
+++ OnlineShopping_CREATE.sql
@@ -1,26 +1,26 @@
 
 CREATE TABLE Supplier(
-SupplierName VARCHAR(255) Unique NOT NULL,
-SupplierID INT PRIMARY KEY,
-ItemsSupplied XML,
-NumeItems INT,
-StoreAdress VARCHAR(255) Unique NOT NUll,
-STOREID INT
+	SupplierName VARCHAR(255) Unique NOT NULL,
+	SupplierID INT PRIMARY KEY,
+	ItemsSupplied XML,
+	NumeItems INT,
+	StoreAdress VARCHAR(255) Unique NOT NUll,
+	STOREID INT
 );
 
 
-create table Catalog(
-ProductID int NOT NULL,
-ProductName varchar(255) NOT NULL,
-Category varchar(255)NOT NULL,
-SKU int NOT NULL,
-Weight decimal(5,2),
-BasePrice decimal(5,2) NOT NULL,
-SalePrice decimal(5,2),
-DietaryInformation XML,
-SoldByWeightorUnit varchar(7) NOT NULL,
-Brand varchar(255),
-QuantityofItem smallint
+CREATE TABLE Catalog(
+	ProductID INT NOT NULL,
+	ProductName VARCHAR(255) NOT NULL,
+	Category VARCHAR(255)NOT NULL,	
+	SKU int NOT NULL,
+	Weight DECIMAL(5,2),
+	BasePrice DECIMAL(5,2) NOT NULL,
+	SalePrice DECIMAL(5,2),
+	DietaryInformation XML,
+	SoldByWeightorUnit VARCHAR(7) NOT NULL,
+	Brand VARCHAR(255),
+	QuantityofItem SMALLINT
 );
 
 
