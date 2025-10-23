fd880b074579fbaf2ceac675dcb7399f837f69b1
ejarredondo <ejarredondo@mail.lipscomb.edu>
2025-10-23 11:29:39 -0500

    added first query

--- Query Statements.sql
+++ Query Statements.sql
@@ -1,4 +1,14 @@
 #What products do we have the least of (per department and overall)?
+
+SELECT ProductName, ProductId, min(QuantityofItem)
+FROM Catalog
+GROUP BY DepartmentID;
+
 #What products are expiring soonest?
+
+
 #What product was sold the most/least during a given month?
+
+
 #Average customer spend and item quantity?
+
