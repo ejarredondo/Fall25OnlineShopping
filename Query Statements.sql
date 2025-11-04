8a94b26c784ce7f244108e68bf01c34408962df5
Elizabeth Carrsco <elizabethcarrsco@Elizabeths-MacBook-Air-2.local>
2025-11-04 13:47:59 -0600

    most/least query

--- Query Statements.sql
+++ Query Statements.sql
@@ -13,6 +13,29 @@ ORDER BY ExpirationDate ASC;
 
 #What product was sold the most/least during a given month?
 
+SELECT C.ProductName,
+	SUM( IS.ItemQuantity) AS TotalQuantotySold
+FROM ItemSupplied IS
+JOIN Transaction T On IS.TransactionID = T.TransactionID
+JOIN Catalog C ON IS.ProductID = C.ProductID
+WHERE T.INCOMINGOROUTGOING = 'O'
+	AND EXTRACT(Year FROM T.TransactionDate) = Default
+Group BY C.ProductName
+Order BY TotalQuantitySold DESC
+LIMIT 1; 
+
+SELECT C.ProductName,
+	SUM( IS.ItemQuantity) AS TotalQuantotySold
+FROM ItemSupplied IS
+JOIN Transaction T On IS.TransactionID = T.TransactionID
+JOIN Catalog C ON IS.ProductID = C.ProductID
+WHERE T.INCOMINGOROUTGOING = 'O'
+	AND EXTRACT(Year FROM T.TransactionDate) = Default
+Group BY C.ProductName
+Order BY TotalQuantitySold ASC
+LIMIT 1;
+
+
 
 
 #Average customer spend and item quantity?
