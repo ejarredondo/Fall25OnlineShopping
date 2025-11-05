5643abf7fcb02eb1ca98202b56a9532a34acdf22
Elizabeth Carrsco <elizabethcarrsco@Elizabeths-MacBook-Air-2.local>
2025-11-04 20:01:42 -0600

    products by Category, restriction, seasonality

--- Query Statements.sql
+++ Query Statements.sql
@@ -1,17 +1,17 @@
-#What products do we have the least of (per department and overall)?
+1.What products do we have the least of (per department and overall)?
 
 SELECT DepartmentID, ProductID, ProductName, MIN(QuantityofItem)
 FROM Catalog
 GROUP BY DepartmentID, ProductID, ProductName;
 
-#What products are expiring soonest?
+2.What products are expiring soonest?
 
 SELECT ProductID, ProductName, ExpirationDate
 FROM Catalog
 WHERE ExpirationDate IS NOT NULL
 ORDER BY ExpirationDate ASC;
 
-#What product was sold the most/least during a given month?
+3.What product was sold the most during a given year? 
 
 SELECT C.ProductName,
 	SUM( IS.ItemQuantity) AS TotalQuantotySold
@@ -23,6 +23,8 @@ WHERE T.INCOMINGOROUTGOING = 'O'
 Group BY C.ProductName
 Order BY TotalQuantitySold DESC
 LIMIT 1; 
+
+4.What product was sold the least during a given year? 
 
 SELECT C.ProductName,
 	SUM( IS.ItemQuantity) AS TotalQuantotySold
@@ -36,9 +38,7 @@ Order BY TotalQuantitySold ASC
 LIMIT 1;
 
 
-
-
-#Average customer spend and item quantity?
+5.Average customer spend and item quantity?
 
 SELECT C.CustomerID, C.FirstName, C.LastName, 
 SUM (CPH.AmountSpent) AS Totalspent,
@@ -52,3 +52,17 @@ JOIN ItemsSupplied IS ON T.TransactionID = IS.TransactionID
 WhHERE T.IncomingOrOutgoing = 'O'
 GROPU BY C.CustomerID, C.FirstName, C.LastName
 ORDER BY AvgSpentPerTransaction DESC;
+
+6.Products by Category,Restrictions, or Seasonality
+
+SELECT C.Productname, C.Category, DI.Restriction AS DietaryRestriction
+CASE
+	WHEN Month(CurDate()) IN (12, 1, 2) Then 'Winter'
+	WHEN Month(CurDate()) IN (3, 4, 5) Then 'Spring'
+	WHEN Month(CurDate()) IN (6, 7, 8) Then 'Summer'
+	WHEN Month(CurDate()) IN (9, 10, 11) Then 'Autumn'
+	ELSE 'Unkown'
+  End AS CurrentSeason
+FROM Catalog C
+LEFT JOIN DietaryInformation DI ON C.ProductID = DI.ProductID
+ORDER BY C.Category, CurrentSeason, C.ProductName;
