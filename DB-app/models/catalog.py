3ffd24773a5ad04f76cfb1413a710296ea87e435
ejarredondo <ejarredondo@mail.lipscomb.edu>
2025-11-13 21:33:15 -0600

    finished store.py and catalog.py

--- DB-app/models/catalog.py
+++ DB-app/models/catalog.py
@@ -1,19 +1,20 @@
 
 from sqlalchemy import func
-from models.schemas import catalog
+from models.schemas import Catalog
 from core import ma, db
 
 def get_catalogs(): 
-    all_catalogs = catalog.query.all()
+    all_catalogs = Catalog.query.all()
     return catalogs_schema.dump(all_catalogs)
 
-def add_catalog(productname, category, SKU, weight, 
-              baseprice, saleprice, soldbyweightorunit, 
-              brand, quantityofitem, departmentid, expirationdate):
-    a = catalog(productname=productname, category = category, 
-                SKU = SKU, weight = weight, baseprice = baseprice, saleprice = saleprice, 
-                soldbyweightorunit = soldbyweightorunit, brand = brand, quantityofitem = quantityofitem, 
-                departmentid = departmentid, expirationdate = expirationdate, last_update=func.now())
+def add_catalog(product_name, category, sku, weight, 
+              base_price, sale_price, sold_by_weight_or_unit, 
+              brand, quantity_of_item, department_id, expiration_date):
+    a = Catalog(product_name = product_name, category = category, 
+                sku = sku, weight = weight, base_price = base_price, 
+                sale_price = sale_price, sold_by_weight_or_unit = sold_by_weight_or_unit, 
+                brand = brand, quantity_of_item = quantity_of_item, department_id = department_id, 
+                expiration_date = expiration_date, last_update=func.now())
     # need to add foreign key for department ID; consult reference on canvas
     db.session.add(a)
     db.session.commit()
@@ -21,13 +22,13 @@ def add_catalog(productname, category, SKU, weight,
 def delete_catalog(id):
 	# Deletes the data on the basis of unique id and 
 	# redirects to home page
-	data = catalog.query.get(id)
+	data = Catalog.query.get(id)
 	db.session.delete(data)
 	db.session.commit()
      
 class CatalogSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = catalog
+        model = Catalog
 
 catalog_schema = CatalogSchema()
 catalogs_schema = CatalogSchema(many=True)
