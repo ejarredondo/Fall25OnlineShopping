4d21ba46305bfd193ecab549af6d04b2d452cb3e
encb04 <elizabethcarrsco@Elizabeth>
2025-11-18 10:16:16 -0600

    DietaryInfo, ItemsSupp, Transaction .py

---
+++ DB-app/models/OnlineShopping.py/DietaryInfo.py
@@ -0,0 +1,26 @@
+from sqlalchemy import ItemSupplied
+from models.schemas import ItemSupllied
+from core import ma, db
+
+def get_DietaryInformation(): 
+    all_DietaryInformation = DietaryInformation.query.all()
+    return DietaryInformation_schema.dump(all_DietaryInformation)
+
+def add_DietaryInformation(Restriction):
+    DI = DietaryInformation(Restriction=Restriction, last_update=func.now())
+    db.session.add(DI)
+    db.session.commit()
+
+def delete_DietaryInformation(Product_ID):
+	# Deletes the data on the basis of unique id and 
+	# redirects to home page
+	data = DietaryInformation.query.get(Product_ID)
+	db.session.delete(data)
+	db.session.commit()
+
+class DietaryInformationSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = DietaryInformation
+
+dietary_information_schema = DietaryInformationSchema()
+dietary_informations_schema = DietaryInformationSchema(many=True)
