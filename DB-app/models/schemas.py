800141fb3a08813464aab455fe0494256fe6ecd9
GHAZI ELHULU <GHAZIELHULU@GMAIL.COM>
2025-11-18 03:42:18 -0600

    <added transaction id column>

--- DB-app/models/schemas.py
+++ DB-app/models/schemas.py
@@ -141,6 +141,7 @@ class CustomerTransaction(db.Model):
 class EmployeeTransaction(db.Model):
 
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.employee_id', ondelete='RESTRICT', onupdate='CASCADE'), primary_key=True, nullable=False)
+    transaction_id = db.Column(db.SmallInteger, db.ForeignKey('transac.transaction_id', ondelete='RESTRICT', onupdate='CASCADE'), primary_key=True, nullable=False)
 
 
 
