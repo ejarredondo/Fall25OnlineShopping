04a44ce70d11c60a5aab5757b19233696f116ca2
encb04 <elizabethcarrsco@Elizabeth>
2025-12-02 10:28:45 -0600

    fixing syntax

--- DB-app/models/Transaction.py
+++ DB-app/models/Transaction.py
@@ -4,34 +4,34 @@ from core import ma, db
 
 
 def get_Transaction(ID):
-    Transaction = Transac.query.get(ID)
-    return Transac
+    Transaction = Transaction.query.get(ID)
+    return Transaction
 
 def get_all_Transaction_by_Employee(CashierEmployee_id):
-     Transac = db.session.query(Transac
-            ).join(Employee, Transac.CashierEmployee_id == Employee.Employee_id
+     Transaction = db.session.query(Transaction
+            ).join(Employee, Transaction.CashierEmployee_id == Employee.Employee_id
             ).filter(Employee.Employee_id == CashierEmployee_id
             ).all()
 
      return Transaction
 
 def get_Transaction_without_Employee():
-    Transac = db.session.query(Transac).filter(
-         Transac.Employee_id.is_(None)
+    Transaction = db.session.query(Transaction).filter(
+         Transaction.Employee_id.is_(None)
     ).all()
 
-    return Transac
+    return Transaction
 
 
-def add_Transac(CashierEmployeeID, IncomingOrOutgoing, TransactionAmount, TransactionDate):
-    T = Transac(CashierEmployeeID=CashierEmployeeID, IncomingOrOutgoing=IncomingOrOutgoing, TransactionAmount=TransactionAmount, TransactionDate=TransactionDate, last_update=func.now())
+def add_Transaction(CashierEmployeeID, IncomingOrOutgoing, TransactionAmount, TransactionDate):
+    T = Transaction(CashierEmployeeID=CashierEmployeeID, IncomingOrOutgoing=IncomingOrOutgoing, TransactionAmount=TransactionAmount, TransactionDate=TransactionDate, last_update=func.now())
     db.session.add(T)
     db.session.commit()
 
-def delete_Transac(Transac_ID):
+def delete_Transaction(Transaction_ID):
 	# Deletes the data on the basis of unique id and 
 	# redirects to home page
-	data = Transac.query.get(Transac_ID)
+	data = Transaction.query.get(Transaction_ID)
 	db.session.delete(data)
 	db.session.commit()
 
